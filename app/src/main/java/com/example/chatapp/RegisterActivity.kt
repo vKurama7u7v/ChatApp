af8f6a40da2b6425a2eb973fec6efb311d5b6cb7
werever-092 <luis-cuv00@live.com>
2023-03-06 19:12:42 -0600

    ChatApp First Design

--- app/src/main/java/com/example/chatapp/RegisterActivity.kt
+++ app/src/main/java/com/example/chatapp/RegisterActivity.kt
@@ -3,11 +3,9 @@ package com.example.chatapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+        setContentView(R.layout.activity_register)
     }
 }
