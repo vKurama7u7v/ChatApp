7078fa5278ce41bc8fdd178755bfacedb5350eec
DanielHF2403 <eduardo.hernandez03@iest.edu.mx>
2023-05-06 21:06:27 -0600

    login y registro
    implementación de lo gin y registro, cambio de color en el texto ya que no se apreciaba bien

--- app/src/main/java/com/example/chatapp/MainActivity.kt
+++ app/src/main/java/com/example/chatapp/MainActivity.kt
@@ -4,9 +4,15 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+
+    private val auth = Firebase.auth
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -19,7 +25,42 @@ class MainActivity : AppCompatActivity() {
 
         val bnLogin = findViewById<Button>(R.id.btLogin)
         bnLogin.setOnClickListener {
-            startActivity(Intent(this, ViewActivity :: class.java))
+            login()
+        }
+
+        checkuser()
+
+    }
+
+    private fun login(){
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+        auth.signInWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                checkuser()
+            } else {
+                task.exception?.let {
+                    Toast.makeText(baseContext, it.message, Toast.LENGTH_LONG).show()
+                }
+            }
         }
     }
+
+    private fun checkuser(){
+        val user = auth.currentUser
+
+        if (user != null){
+            val s = Intent(this, ViewActivity::class.java)
+            s.putExtra("user", user.email)
+            startActivity(s)
+
+            finish()
+        }
+    }
+
+
 }
--- app/src/main/java/com/example/chatapp/RegisterActivity.kt
+++ app/src/main/java/com/example/chatapp/RegisterActivity.kt
@@ -3,9 +3,17 @@ package com.example.chatapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class RegisterActivity : AppCompatActivity() {
+
+    private val auth = Firebase.auth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
@@ -14,5 +22,41 @@ class RegisterActivity : AppCompatActivity() {
         tvLogin.setOnClickListener {
             startActivity(Intent(this, MainActivity :: class.java))
         }
+
+        val btlogin = findViewById<Button>(R.id.btLogin)
+        btlogin.setOnClickListener{
+            registarse()
+        }
     }
+
+    private fun registarse(){
+        val etEmail = findViewById<EditText>(R.id.etEmail)
+        val etPassword = findViewById<EditText>(R.id.etPassword)
+
+        val email = etEmail.text.toString()
+        val password = etPassword.text.toString()
+
+        auth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+            if (task.isSuccessful){
+                checkuser()
+            } else {
+                task.exception?.let {
+                    Toast.makeText(baseContext, it.message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private fun checkuser(){
+        val user = auth.currentUser
+
+        if (user != null){
+            val s = Intent(this, ViewActivity::class.java)
+            s.putExtra("user", user.email)
+            startActivity(s)
+
+            finish()
+        }
+    }
+
 }
