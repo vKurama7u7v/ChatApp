cb3562f826d1921bfb55ee92e1679f0f509a7778
Aurelio Marín <aurelio.marin@iest.edu.mx>
2023-05-11 19:39:55 -0600

    add send message

--- app/src/main/java/com/example/chatapp/activities/ChatActivity.kt
+++ app/src/main/java/com/example/chatapp/activities/ChatActivity.kt
@@ -2,11 +2,78 @@ package com.example.chatapp.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.chatapp.R
+import com.example.chatapp.adapters.MessageAdapter
+import com.example.chatapp.models.Message
+import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class ChatActivity : AppCompatActivity() {
+    private var chatId = ""
+    private var user = ""
+
+    private lateinit var messageTextField: EditText
+    private lateinit var sendMessageButton: Button
+
+    private lateinit var messagesRecylerView: RecyclerView
+    private lateinit var layoutManager: RecyclerView.LayoutManager
+
+    private var db = Firebase.firestore
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_chat)
+
+        intent.getStringExtra("chatId")?.let { chatId = it }
+        intent.getStringExtra("user")?.let { user = it }
+
+        if(chatId.isNotEmpty() && user.isNotEmpty()) {
+            initViews()
+        }
+    }
+
+    private fun initViews(){
+        messageTextField = findViewById(R.id.messageTextField)
+        sendMessageButton = findViewById(R.id.sendMessageButton)
+
+        messagesRecylerView = findViewById(R.id.messagesRecylerView)
+        messagesRecylerView.layoutManager = LinearLayoutManager(this)
+        messagesRecylerView.adapter = MessageAdapter(user)
+
+        sendMessageButton.setOnClickListener { sendMessage() }
+
+        val chatRef = db.collection("chats").document(chatId)
+
+        chatRef.collection("messages").orderBy("dob", Query.Direction.ASCENDING)
+            .get()
+            .addOnSuccessListener { messages ->
+                val listMessages = messages.toObjects(Message::class.java)
+                (messagesRecylerView.adapter as MessageAdapter).setData(listMessages)
+            }
+
+        chatRef.collection("messages").orderBy("dob", Query.Direction.ASCENDING)
+            .addSnapshotListener { messages, error ->
+                if(error == null){
+                    messages?.let {
+                        val listMessages = it.toObjects(Message::class.java)
+                        (messagesRecylerView.adapter as MessageAdapter).setData(listMessages)
+                    }
+                }
+            }
+    }
+
+    private fun sendMessage(){
+        val message = Message(
+            message = messageTextField.text.toString(),
+            from = user
+        )
+
+        db.collection("chats").document(chatId).collection("messages").document().set(message)
+        messageTextField.setText("")
     }
 }
--- app/src/main/java/com/example/chatapp/adapters/MessageAdapter.kt
+++ app/src/main/java/com/example/chatapp/adapters/MessageAdapter.kt
@@ -1,4 +1,52 @@
 package com.example.chatapp.adapters
 
-class MessageAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+import com.example.chatapp.R
+import com.example.chatapp.models.Message
+
+class MessageAdapter(private val user: String): RecyclerView.Adapter<MessageAdapter.MessageViewHolder>(){
+    private var messages: List<Message> = emptyList()
+
+    fun setData(list: List<Message>){
+        messages = list
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageViewHolder {
+        return MessageViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_message,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: MessageViewHolder, position: Int) {
+        val message = messages[position]
+
+        if(user == message.from){
+            holder.itemView.findViewById<ConstraintLayout>(R.id.myMessageLayout).visibility = View.VISIBLE
+            holder.itemView.findViewById<ConstraintLayout>(R.id.otherMessageLayout).visibility = View.GONE
+
+            holder.itemView.findViewById<TextView>(R.id.myMessageTextView).text = message.message
+        } else {
+            holder.itemView.findViewById<ConstraintLayout>(R.id.myMessageLayout).visibility = View.GONE
+            holder.itemView.findViewById<ConstraintLayout>(R.id.otherMessageLayout).visibility = View.VISIBLE
+
+            holder.itemView.findViewById<TextView>(R.id.othersMessageTextView).text = message.message
+        }
+
+    }
+
+    override fun getItemCount(): Int {
+        return messages.size
+    }
+
+    class MessageViewHolder(itemView: View): RecyclerView.ViewHolder(itemView)
 }
